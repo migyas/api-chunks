f9b335529cc075e591e9ec7a985d84073718ffcd
Yan Santos <yansantos0712@gmail.com>
2023-07-11 16:19:17 -0300

    feat: zod validations

--- src/http/controllers/products/create.ts
+++ src/http/controllers/products/create.ts
@@ -1,29 +1,33 @@
 import { makeCreateProductUseCase } from "@/use-cases/factories/make-create-product-use-case";
 import { FastifyReply, FastifyRequest } from "fastify";
-import { z } from "zod";
+import { ZodError, z } from "zod";
+import { fromZodError } from "zod-validation-error";
 
 export async function create(request: FastifyRequest, reply: FastifyReply) {
   const createBodySchema = z.array(
     z.object({
       data_preco: z.coerce.date(),
-      cod_produto: z.number(),
+      cod_produto: z.number().int(),
       sku: z.string(),
-      qtd_estoque: z.number(),
+      qtd_estoque: z.number().int().positive(),
       desconto: z.number(),
       data_inicio: z.coerce.date(),
       data_fim: z.coerce.date(),
     })
   );
 
-  const products = createBodySchema.parse(request.body);
-
   try {
+    const products = createBodySchema.parse(request.body);
     const createProductUseCase = makeCreateProductUseCase();
 
     await createProductUseCase.execute(products);
   } catch (err) {
-    console.log((err as Error).message);
-    throw err;
+    const { details, name } = fromZodError(err as ZodError);
+
+    return reply.status(400).send({
+      name,
+      details,
+    });
   }
 
   return reply.status(201).send();
--- src/repositories/prisma/prisma-products-repository.ts
+++ src/repositories/prisma/prisma-products-repository.ts
@@ -20,6 +20,7 @@ export class PrismaProductsRepository implements ProductsRepository {
         });
       })
     );
+
     return products;
   }
 }
