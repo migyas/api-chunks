ebcf44176c677bd9c3ab040f6d576ed2d286576d
Yan Santos <yansantos0712@gmail.com>
2023-07-16 05:05:00 -0300

    feat: create a less data in database

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,26 @@
 import fastify from "fastify";
+import fastifyMultipart from "@fastify/multipart";
+import fastifyFormidable from "fastify-formidable";
+
 import { mainRoutes } from "./http/controllers/main/routes";
 import { ZodError } from "zod";
 import { env } from "./env";
 import { productsRoutes } from "./http/controllers/products/routes";
 
-export const app = fastify();
+export const app = fastify({
+  bodyLimit: 1024 * 1024 * 100, // Aumenta o limite do payload para 100 MB (valor em bytes)
+  connectionTimeout: 60000,
+});
 
+// app.register(fastifyFormidable, { addContentTypeParser: true });
+app.register(fastifyMultipart);
 app.register(mainRoutes);
 app.register(productsRoutes);
 
+app.addHook("preHandler", (req, repy, done) => {
+  repy.header("Content-type", "application/json");
+  done();
+});
 app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
     return reply
--- src/http/controllers/products/create.ts
+++ src/http/controllers/products/create.ts
@@ -2,6 +2,12 @@ import { makeCreateProductUseCase } from "@/use-cases/factories/make-create-prod
 import { FastifyReply, FastifyRequest } from "fastify";
 import { ZodError, z } from "zod";
 import { fromZodError } from "zod-validation-error";
+import { WriteStream, createReadStream } from "node:fs";
+import csv from "csvtojson";
+import { Transform, Readable, PassThrough } from "node:stream";
+import { pipeline } from "node:stream/promises";
+import BJSON from "buffer-json";
+import formidable from "formidable";
 
 export async function create(request: FastifyRequest, reply: FastifyReply) {
   const createBodySchema = z.array(
@@ -16,19 +22,158 @@ export async function create(request: FastifyRequest, reply: FastifyReply) {
     })
   );
 
+  // for await (const file of request.files()) {
+  //   const toBufferFile = await file.toBuffer();
+  //   const parsedFiles = JSON.parse(toBufferFile.toString());
+
+  //   try {
+  //     const products = createBodySchema.parse(parsedFiles);
+  //     const createProductUseCase = makeCreateProductUseCase();
+
+  //     await createProductUseCase.execute(products);
+  //     return reply.status(201).send();
+  //   } catch (err) {
+  //     const { details, name } = fromZodError(err as ZodError);
+
+  //     return reply.status(400).send({
+  //       name,
+  //       details,
+  //     });
+  //   }
+  // }
+
+  // readableStreamTest
+  //   .pipe(csv({ delimiter: "," }, { objectMode: true }))
+  //   .on("data", (data) => console.log(data));
+
+  // const readableStreamTest = createReadStream("tasks.csv");
+  const readableStream = request.raw;
+  const writeableStream = reply.raw;
+
+  // readableStream.setEncoding("utf8");
+
+  // console.log("TO AQUI", readTest);
   try {
-    const products = createBodySchema.parse(request.body);
-    const createProductUseCase = makeCreateProductUseCase();
-
-    await createProductUseCase.execute(products);
-  } catch (err) {
-    const { details, name } = fromZodError(err as ZodError);
-
-    return reply.status(400).send({
-      name,
-      details,
-    });
+    await pipeline(
+      readableStream,
+      new Transform({
+        objectMode: true,
+        transform(chunk, encoding, callback) {
+          console.log("chunk", chunk.toString());
+          callback(null, JSON.stringify(chunk));
+        },
+      })
+    );
+    console.log("Streaming Ended =)");
+  } catch (error) {
+    console.log("Streaming Ended with Error", error);
   }
 
-  return reply.status(201).send();
+  // readableStream
+
+  //   .on("data", (data) => {
+  //     const parsedChunk = JSON.parse(data.toString());
+  //     console.log(parsedChunk);
+  //   })
+  //   .on("error", (err) => console.error(err))
+  //   .on("end", () => console.log("Streaming Ended"));
+
+  // readableStream.pipe();
+  // readableStream.on("data", (buffer) => console.log(buffer));
+  // readableStream.pipe(writeableStream);
+
+  // const writeableStream = request.multipart(
+  //   (handler) => console.log("multpart", handler),
+  //   (err) => console.error(err),
+  //   { highWaterMark: 1000 }
+  // );
+
+  // request.raw.on("data", (buffer) => writeableStream.write(buffer.toString()));
+
+  // request.raw.on("end", () => {
+  //   console.log("Ended");
+  //   writeableStream.end();
+  // });
+
+  // writeableStream.on("file", (_, file) => console.log(file));
+  // console.log();
+  // type ProductSchema = z.infer<typeof createBodySchema>;
+  // await request.file({ highWaterMark: 10 }).then((data) => {
+  //   data?.file.on("data", async (chunk) => {
+  //     console.log(chunk);
+  //   });
+
+  //   data?.file.on("end", () => {});
+  // });
+
+  // stream?.file.on("data", (chunk) => products.push(chunk.toString()));
+
+  // stream?.file.on("end", () => {
+  //   const parsedBuffers = JSON.parse(products.toString());
+
+  //   console.log(parsedBuffers);
+  //   request.file().
+  // });
+
+  // const products = createBodySchema.parse(
+  //   JSON.parse((request.body as any).products[0].data.toString())
+  // );
+
+  // const mp = request.multipart(handler, (err) => {
+  //   console.log(err);
+  // });
+  // console.log(mp);
+
+  // async function handler(field, file, filename, encoding, mimetype) {
+  //   // Lida com o arquivo enquanto está sendo transmitido
+  //   const writeStream = createWriteStream(`uploads/${filename}`);
+
+  //   file.on("data", (chunk) => {
+  //     // Faz algo com os dados do arquivo chunk a chunk
+  //     writeStream.write(chunk);
+  //   });
+
+  //   file.on("end", () => {
+  //     // Finaliza o processamento do arquivo
+  //     writeStream.end();
+  //   });
+  // }
+
+  // const data = request.parts();
+  // const products = createBodySchema.parse(request.body);
+  // if (data) {
+  //   // TEMOS OS DADOS DO ARQUIVO SENDO LIDO DE FORMA CORRETA
+  //   data.file; // stream
+  //   data.fields; // other parsed parts
+  //   data.fieldname;
+  //   data.filename;
+  //   data.encoding;
+  //   data.mimetype;
+
+  //   console.log(JSON.parse(data.file.read().toString()));
+
+  //   // await pump(data.file, fs.createWriteStream(data.filename));
+  // }
+
+  // request.raw.on("end", async () => {
+  //   const concated = JSON.parse(Buffer.concat(buffers).toString());
+  //   // const string = concated.toString();
+  //   // const jsonString = JSON.parse(string);
+  //   console.log("TO AQUII", await processJSON(concated));
+
+  //   // products.push(JSON.parse(resultString));
+  // });
+
+  // try {
+  //   const createProductUseCase = makeCreateProductUseCase();
+  //   // console.log(products);
+  //   // await createProductUseCase.execute(products);
+  // } catch (err) {
+  //   const { details, name } = fromZodError(err as ZodError);
+
+  //   return reply.status(400).send({
+  //     name,
+  //     details,
+  //   });
+  // }
 }
